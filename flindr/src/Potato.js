1b50f789c5b00b06b7e5296c3fed733447222383
Dainius <DVARANAUSKAS7@GMAIL.COM>
2018-11-10 22:12:56 +0000

    Trip display and Buttons
    Added trip display and yes no functionality. Use it for fruther implimentation for the backend.

---
+++ flindr/src/Potato.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+/*const Areas = [
+  {id: "1", uri: require('./assets/images/1.png')},
+  {id: "2", uri: require('./assets/images/2.png')},
+  {id: "3", uri: require('./assets/images/3.png')},
+]*/
+
+class Potato extends Component {
+  render() {
+    return (
+      <div className="Potato">
+        AEtataet
+      {/*<div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>*/}
+    </div>      
+    );
+  }
+}
+
+export default Potato;
